d153affe6e2171ce0ffd1d2ecca51196a2de96ca
Harsh Bodgal <harshbodgal22@gmail.com>
2024-09-02 23:49:40 -0700

    fixing-route-conflict-for-vercel

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,13 @@
-import ViewStreams from "./(viewStream)/page";
+import { createClient } from "@/utils/supabase/server";
+import FetchStreams from "./(viewStream)/fetch-streams";
+export const revalidate = 0;
 export default async function Index() {
-
+  const supabase = createClient();
+  const { data } = await supabase.from("streams").select();
   return (
     <>
       <main className="flex-1 flex flex-row gap-6 px-4">
-        <ViewStreams />
+        <FetchStreams activeStreams={data ?? []} />
       </main>
     </>
   );
